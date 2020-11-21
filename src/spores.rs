113a77755c5c9eeff950071d3d858b100f156425
Max Ilinov <ilimaksim@gmail.com>
2020-11-22 00:16:50 +0300

    insert new spore

--- src/spores.rs
+++ src/spores.rs
@@ -2,7 +2,7 @@ use date_time::date_tuple::DateTuple;
 use date_time::time_tuple::Duration;
 
 #[derive(Debug, PartialEq)]
-pub struct Spores<'a> (Vec<&'a Spore<'a>>);
+pub struct Spores<'a> (Vec<Spore<'a>>);
 
 #[derive(Debug, PartialEq)]
 pub struct Spore<'a> {
@@ -15,11 +15,30 @@ pub struct Spore<'a> {
     pub state: SporeState,
 }
 
-impl<'a> Spores<'_>  {
+impl Spores<'static> {
+    
     pub fn new() -> Spores<'static> {
         Spores (Vec::new())
-        }
     }
+
+    pub fn insert_spore(&mut self, tag: String) {
+        let spore: Spore = Spore {
+            tag: tag.clone(),
+            id: tag.into_bytes().iter().copied().map(u16::from).sum::<u16>(),
+            next: vec![],
+            prev: vec![],
+            date: DateTuple::today(),
+            dur: Duration::new(01, 00, 00),
+            state: SporeState::DoState,
+        };
+        &self.0.push(spore);
+    }
+
+    pub fn len(self) -> usize {
+        self.0.len()
+    }
+
+}
 
 #[derive(Debug, PartialEq)]
 pub enum SporeState {
@@ -36,6 +55,13 @@ mod tests {
     fn test_init_spores() {
         let spores = Spores::new();
         assert_eq!( spores, Spores::new())
+    }
+
+    #[test]
+    fn test_insert_spore() {
+        let mut spores = Spores::new();
+        spores.insert_spore("new_spore".to_string());
+        assert_eq!(1, spores.len())
     }
 }
 
